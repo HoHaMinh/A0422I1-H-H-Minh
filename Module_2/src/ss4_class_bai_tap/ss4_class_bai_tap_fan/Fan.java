f1290eb6b0a87ce9f61be6bdbf2cb17d22928f56
HoHaMinh <hohaminh88@gmail.com>
2022-07-20 19:20:47 +0700

    ss4

---
+++ Module_2/src/ss4_class_bai_tap/ss4_class_bai_tap_fan/Fan.java
@@ -0,0 +1,66 @@
+package ss4_class_bai_tap.ss4_class_bai_tap_fan;
+
+public class Fan {
+    private final int SLOW = 1;
+    private final int MEDIUM = 2;
+    private final int FAST = 3;
+    private int speed = 1;
+    private boolean isTurnOn = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public Fan() {
+    }
+
+    public int getSLOW() {
+        return SLOW;
+    }
+
+    public int getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public int getFAST() {
+        return FAST;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isTurnOn() {
+        return isTurnOn;
+    }
+
+    public void setTurnOn(boolean turnOn) {
+        isTurnOn = turnOn;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString() {
+        if (isTurnOn) {
+            return "Fan is on with speed is: " + speed + ", color is: " + color + ", radius is: " + radius;
+        } else {
+            return "Fan is off with color is: " + color + ", radius is: " + radius;
+        }
+    }
+}
