67270d846a143d02e90cbcbfc27d0aaac8b776e1
HoHaMinh <hohaminh88@gmail.com>
2022-09-18 23:12:26 +0700

    ss1

---
+++ Module_3/ss1_relational_database/exercise.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE my_database;
+DROP database my_database;
+create database `student-management`;
+use `student-management`;
+create table student (
+`id` int,
+`name` varchar(45),
+age int,
+country varchar(45));
+select * from student;
+create table class (
+id int,
+`name` varchar(45));
+create table teacher(
+id int,
+`name` varchar(45),
+age int,
+country varchar(45));
