2e7149a8216395a36df342ec26b563aa28301739
HoHaMinh <hohaminh88@gmail.com>
2022-09-26 19:19:02 +0700

    ss3 fixed

--- Module_3/ss3_database_query/exercises/query_sale_management.sql
+++ Module_3/ss3_database_query/exercises/query_sale_management.sql
@@ -24,18 +24,21 @@ create table order_detaile (
     foreign key (product_id) references product (product_id),
     order_quantity double not null
 );
+
 INSERT INTO
     customer (customer_name, customer_age)
 VALUES
     ('Minh Quan', 10),
     ('Ngoc Oanh', 20),
     ('Hong Ha', 50);
+    
 INSERT INTO
     `order` (customer_id, order_date, order_total_price)
 VALUES
     (1, '2006/03/21', NULL),
     (2, '2006/03/23', NULL),
     (1, '2006/03/16', NULL);
+    
 INSERT INTO
     product (product_name, product_price)
 VALUES
@@ -44,6 +47,7 @@ VALUES
     ('Dieu Hoa', 7),
     ('Quat', 1),
     ('Bep Dien', 2);
+    
 INSERT INTO
     order_detaile
 VALUES
@@ -54,6 +58,7 @@ VALUES
     (3, 1, 8),
     (2, 5, 4),
     (2, 2, 3);
+    
 SELECT
     order_id,
     order_date,
@@ -67,6 +72,7 @@ FROM
     customer C
     JOIN `order` O on O.customer_id = C.customer_id
     JOIN order_detaile OD on OD.order_id = O.order_id;
+    
 SELECT
     C.customer_name
 FROM
@@ -77,7 +83,8 @@ WHERE
             O.customer_id
         FROM
             `order` O
-    )
+    );
+    
 SELECT
     OD.order_id,
     sum(OD.product_id * OD.order_quantity) as 'Total value'
